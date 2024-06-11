ac39ef90808830df314b2c2c2c2e54bbac7016ac
thezulux24 <thezulux24gmail.com>
2024-06-10 22:15:40 -0500

    version beta

--- main.py
+++ main.py
@@ -104,18 +104,13 @@ def page2():
         else:
             st.error("Por favor, llena todos los campos antes de enviar.")
 def main():
-    st.markdown(
-        """
+    st.markdown("""
         <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
-        """,
-        unsafe_allow_html=True
-    )
+        """, unsafe_allow_html=True)
+    
     if "page" not in st.session_state:
         st.session_state.page = 1
 
