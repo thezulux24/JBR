da337bd86ca8054b14846c70c4c5d3dc9567b2a5
thezulux24 <thezulux24gmail.com>
2024-06-20 10:05:17 -0500

    updated

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ from google.oauth2 import service_account
 
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-KEY = st.secrets("key_path")
-SPREADSHEET_ID = st.secrets("spreedsheet")
+KEY = st.secrets["key_path"]
+SPREADSHEET_ID = st.secrets["spreedsheet"]
+
 
 creds = None
 creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
 service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
-email_user = st.secrets("email_user")
-email_password = st.secrets("email_password")
-recep = st.secrets("recep")
+email_user = st.secrets["email_user"]
+email_password = st.secrets["email_password"]
+recep = st.secrets["recep"]
 
 
 
